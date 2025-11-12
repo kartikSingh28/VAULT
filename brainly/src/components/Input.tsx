45da449aecc3bb4d776463ad3347becfee46e3aa
kartik <kaxxtric001@gmail.com>
2025-11-12 20:26:12 +0530

    ContentModel And SideBar .tsx

---
+++ brainly/src/components/Input.tsx
@@ -0,0 +1,17 @@
+interface InputProps {
+    placeholder:string,
+    reference?:any
+}
+
+export function Input({placeholder,reference}: InputProps){
+    return (
+        <>
+           <input 
+              ref={reference}
+              placeholder={placeholder}
+              type={"text"}
+              className="px-4 py-2 border rounded m-2" 
+               />
+        </>
+    )
+}
